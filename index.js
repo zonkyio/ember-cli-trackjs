41928b4d338466ad07d57807b1a26fa35b7cb202
Jaromir Kratochvil <jaromir.kratochvil@zonky.cz>
2018-10-01 13:07:44 +0200

    node compatibility problem solved

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var path = require('path');
+var fbTransform = require('fastboot-transform');
+
 module.exports = {
   name: require('./package').name,
 
@@ -18,9 +23,33 @@ module.exports = {
     }
   },
 
-  included(app) {
-    this._super.included(app);
+  treeForVendor(defaultTree) {
+    var trees = [];
 
-    app.import(require.resolve('trackjs'));
+		if (defaultTree) {
+			trees.push(defaultTree);
+    }
+    
+    let trackJsPath = path.join(this.project.root, 'node_modules', 'trackjs');
+
+    let browserVendorLib = fbTransform(new Funnel(trackJsPath, {
+      files: ['tracker.js'],
+      destDir: 'trackjs',
+    }));
+  
+    trees.push(browserVendorLib);
+    
+    return mergeTrees(trees);
+  },
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    // allow addon to be nested - see: https://github.com/ember-cli/ember-cli/issues/3718
+    while (app.app) {
+      app = app.app;
+    }
+
+    app.import(this.treePaths.vendor + '/trackjs/tracker.js');
   }
 };
